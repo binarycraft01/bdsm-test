0a5655a9585832a75b85467be66120f0a5008bd4
minsu <binarycraft01@gmail.com>
2026-02-27 21:34:00 +0900

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +12,8 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://bdsm-test-pink.vercel.app"),
